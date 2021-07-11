dc58999b8c11feef7b1533905001e0601cd0bcc0
redstonecold <hsc010616@gmail.com>
2021-07-11 11:05:24 +0900

    2021-07-11 11:05

--- Week1/n2-1_HongSeokchan_20210706.py
+++ Week1/n2-1_HongSeokchan_20210706.py
@@ -1,56 +1,27 @@
-vowelList = ['a', 'e', 'i', 'o', 'u']
-ans = []
-
-while True : 
-    password = input() 
-    if password == 'end' : break #end일 시 break
-    
-    checkVOWEL = False
-    continuity = 0
-    preword = ''
-    proper = True
-
-    for word in password : #문자열에서 한 문자씩 비교
-        if word == preword and word+preword != 'ee' and word+preword != 'oo' : proper = False #같은 글자가 연속적으로 두번 오면 proper=false (ee 와 oo는 제외).
-        if word in vowelList : #해당 문자가 모음일 때
-            checkVOWEL = True 
-            if preword in vowelList : #이전 문자가 모음이면 continuity + 1
-                continuity = continuity + 1
-            else : continuity = 0 #자음이면 continuity = 0
-        else : #해당 문자가 자음일 때
-            if preword not in vowelList : #이전 문자가 자음이면 continuity + 1
-                continuity = continuity + 1
-            else : continuity = 0 #모음이면 continuity = 0
-        preword = word #다음 문자로 넘어가기전 현재 문자를 이전 문자로 설정
-        if continuity > 1 : proper = False #같은 문자가 3번 이상 나오면 proper = False
-
-    if checkVOWEL == False : proper = False #모음이 없었으면 proper = False
-
-    if proper :
-        ans.append("<%s> is acceptable."%password)
-    else :
-        ans.append("<%s> is not acceptable."%password)
-
-for str in ans :
-    print(str)
-    
 '''
-l = "aeiou"
-while True:
+https://www.acmicpc.net/status?user_id=hsch01&problem_id=4659&from_mine=1 
+'''
+v = 'aeiou'
+while True :
+    suit = True
+    vnum = 0
+
     s = input()
-    chk,chk2 = 0,0
-    if s == "end": break;
-    for i in s:
-        if i in l:
-            chk2=1
-    #print(chk2)
-    for i in range(1,len(s)):
-        if s[i] == s[i-1] and s[i] not in 'eo': chk=1
-    #print(chk)
-    for i in range(len(s)-2):
-        if s[i] in l and s[i+1] in l and s[i+2] in l: chk=1
-        elif s[i] not in l and s[i+1] not in l and s[i+2] not in l: chk=1
-    #print(chk)
-    if chk == 1 or chk2 == 0: print("<" + s + "> is not acceptable." )
-    else: print("<" + s + "> is acceptable." 
-'''
+    if s == 'end' : break
+    for i in range(len(s)-2) : 
+        if s[i] in v and s[i+1] in v and s[i+2] in v : #모음이 3개 연속 나오는지 확인
+            suit = False
+        elif s[i] not in v and s[i+1] not in v and s[i+2] not in v : #자음이 3개 연속 나오는지 확인
+            suit = False
+    for i in range(len(s)-1) :
+        if s[i] == s[i+1] and s[i] not in 'eo' and s[i+1] not in 'eo' : #ee oo가 아닌 같은 글자 두 번 연속이 있는지 확인
+            suit = False
+    for ch in s : # 자음이 하나 이상있는지 확인
+        if ch in v :
+            vnum += 1
+    if vnum <= 0 :
+        suit = False
+    
+    if suit == False :
+        print("<%s> is not acceptable."%s)
+    else : print("<%s> is acceptable."%s)
